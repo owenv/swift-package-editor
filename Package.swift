36b527f6a8ef2c6c062935c07c7e381116e828ab
Owen Voorhees <owen@squibner.com>
2021-07-31 13:48:33 -0700

    Initial import

--- Package.swift
+++ Package.swift
@@ -1,28 +1,68 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
 
 import PackageDescription
+import Foundation
 
 let package = Package(
     name: "swift-package-editor",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "swift-package-editor",
-            targets: ["swift-package-editor"]),
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .executable(
+            name: "swift-package-editor",
+            targets: ["swift-package-editor"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "swift-package-editor",
-            dependencies: []),
-        .testTarget(
-            name: "swift-package-editorTests",
-            dependencies: ["swift-package-editor"]),
+        .executableTarget(name: "swift-package-editor", dependencies: [
+            "PackageSyntax",
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+            .product(name: "SwiftPM-auto", package: "swift-package-manager"),
+        ]),
+        .target(name: "PackageSyntax", dependencies: [
+            .product(name: "SwiftSyntax", package: "swift-syntax"),
+            .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+            .product(name: "SwiftPM-auto", package: "swift-package-manager")
+        ]),
+        .testTarget(name: "PackageSyntaxTests", dependencies: [
+            "PackageSyntax",
+            .product(name: "SwiftSyntax", package: "swift-syntax"),
+            .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+            .product(name: "SwiftPM-auto", package: "swift-package-manager"),
+            .product(name: "PackageDescription", package: "swift-package-manager")
+        ])
     ]
 )
+
+let relatedDependenciesBranch = "main"
+
+if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
+    package.dependencies += [
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch(relatedDependenciesBranch)),
+        // The 'swift-argument-parser' version declared here must match that
+        // used by 'swift-driver', 'sourcekit-lsp', and 'swiftpm'. Please coordinate
+        // dependency version changes here with those projects.
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.4.3")),
+        .package(url: "https://github.com/apple/swift-package-manager.git", .branch(relatedDependenciesBranch)),
+        .package(url: "https://github.com/apple/swift-syntax.git", .branch(relatedDependenciesBranch))
+    ]
+} else {
+    package.dependencies += [
+        .package(path: "../swift-tools-support-core"),
+        .package(path: "../swift-argument-parser"),
+        .package(path: "../swiftpm"),
+        .package(path: "../swift-syntax")
+    ]
+}
--- Sources/swift-package-editor/swift_package_editor.swift
+++
@@ -1,6 +0,0 @@
-public struct swift_package_editor {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
